d8bbffd2ded5e527f71d066efee8e1272b3b1195
Oleksandr Mashura <am@amc>
2022-02-15 13:27:09 +0200

    fix: :lipstick: Fixed scrolling during the modal popups
    #1

--- js/form.js
+++ js/form.js
@@ -12,12 +12,16 @@ function closeModals(e) {
 
     modalForm.classList.remove(MODAL_ACTIVE_CLASS);
     modalSuccess.classList.remove(MODAL_ACTIVE_CLASS);
+
+    document.body.classList.remove('body-fixed');
 }
 
 function openSuccessModal() {
     modalForm.classList.remove(MODAL_ACTIVE_CLASS);
     modalSuccess.classList.add(MODAL_ACTIVE_CLASS);
 
+    document.body.classList.add('body-fixed');
+    
     const modalFormClose = document.querySelector('#modal-success-close');
     modalFormClose.addEventListener('click', closeModals);
 };
@@ -42,6 +46,8 @@ callFormBtn.addEventListener('click', function () {
 
     const modalFormClose = document.querySelector('#modal-form-close');
     modalFormClose.addEventListener('click', closeModals);
+
+    document.body.classList.add('body-fixed');
 
     form.addEventListener('submit', sendUserInfo);
 });
